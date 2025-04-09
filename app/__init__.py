e650b47540ac338de11c17741c812eb829cd1e4f
amirdsmedia <006khan@gmail.com>
2025-04-09 12:47:55 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
     from .views import main
     app.register_blueprint(main)
     return app
